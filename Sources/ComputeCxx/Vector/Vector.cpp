af96369e19b37b11c624902b01e2b3091aa5096d
James Moschou <james.moschou@gmail.com>
2025-01-07 23:49:24 +1100

    Add vector class

---
+++ Sources/ComputeCxx/Vector/Vector.cpp
@@ -0,0 +1,262 @@
+#include "Vector.h"
+
+#include "Errors/Errors.h"
+
+namespace AG {
+
+#pragma mark - Base implementation
+
+namespace details {
+
+template <typename size_type, unsigned int element_size>
+    requires std::unsigned_integral<size_type>
+void *realloc_vector(void *buffer, void *stack_buffer, size_type stack_size, size_type *size,
+                     size_type preferred_new_size) {
+    // copy data from heap buffer buffer into stack buffer if possible
+    if (preferred_new_size <= stack_size) {
+        if (buffer) {
+            memcpy(stack_buffer, buffer, preferred_new_size * element_size);
+            free(buffer);
+            *size = stack_size;
+        }
+        return nullptr;
+    }
+
+    size_t new_size_bytes = malloc_good_size(preferred_new_size * element_size);
+    size_type new_size = new_size_bytes / element_size;
+    if (new_size == *size) {
+        // nothing to do
+        return buffer;
+    }
+
+    void *new_buffer = realloc(buffer, new_size_bytes);
+    if (!new_buffer) {
+        precondition_failure("allocation failure");
+    }
+
+    // copy data from stack buffer into heap buffer
+    if (!buffer) {
+        memcpy(new_buffer, stack_buffer, (*size) * element_size);
+    }
+
+    *size = new_size;
+    return new_buffer;
+}
+
+} // namespace details
+
+template <typename T, unsigned int _stack_size, typename size_type>
+    requires std::unsigned_integral<size_type>
+vector<T, _stack_size, size_type>::~vector() {
+    for (auto i = 0; i < _size; i++) {
+        delete this[i];
+    }
+    if (_buffer) {
+        free(_buffer);
+    }
+}
+
+template <typename T, unsigned int _stack_size, typename size_type>
+    requires std::unsigned_integral<size_type>
+void vector<T, _stack_size, size_type>::reserve_slow(size_type new_cap) {
+    size_type effective_new_cap = max(capacity() * 1.5, new_cap);
+    _buffer = details::realloc_vector(_buffer, _stack_buffer, _stack_size, &_capacity, effective_new_cap);
+}
+
+template <typename T, unsigned int _stack_size, typename size_type>
+    requires std::unsigned_integral<size_type>
+void vector<T, _stack_size, size_type>::reserve(size_type new_cap) {
+    if (new_cap <= capacity()) {
+        return;
+    }
+    reserve_slow(new_cap);
+}
+
+template <typename T, unsigned int _stack_size, typename size_type>
+    requires std::unsigned_integral<size_type>
+void vector<T, _stack_size, size_type>::clear() {
+    for (auto i = 0; i < _size; i++) {
+        delete this[i];
+    }
+    _size = 0;
+}
+
+template <typename T, unsigned int _stack_size, typename size_type>
+    requires std::unsigned_integral<size_type>
+void vector<T, _stack_size, size_type>::push_back(const T &value) {
+    reserve(_size + 1);
+    new (this[_size]) value_type(value);
+    _size += 1;
+}
+
+template <typename T, unsigned int _stack_size, typename size_type>
+    requires std::unsigned_integral<size_type>
+void vector<T, _stack_size, size_type>::push_back(T &&value) {
+    reserve(_size + 1);
+    new (this[_size]) value_type(std::move(value));
+    _size += 1;
+}
+
+template <typename T, unsigned int _stack_size, typename size_type>
+    requires std::unsigned_integral<size_type>
+void vector<T, _stack_size, size_type>::pop_back() {
+    assert(size() > 0);
+    delete this[_size - 1];
+    _size -= 1;
+}
+
+template <typename T, unsigned int _stack_size, typename size_type>
+    requires std::unsigned_integral<size_type>
+void vector<T, _stack_size, size_type>::resize(size_type count) {
+    reserve(count);
+    if (count < _size) {
+        for (auto i = count; i < _size; i++) {
+            delete this[i];
+        }
+    } else if (count > _size) {
+        for (auto i = _size; i < count; i++) {
+            new (this[i]) value_type();
+        }
+    }
+    _size = count;
+}
+
+template <typename T, unsigned int _stack_size, typename size_type>
+    requires std::unsigned_integral<size_type>
+void vector<T, _stack_size, size_type>::resize(size_type count, const value_type &value) {
+    reserve(count);
+    if (count < _size) {
+        for (auto i = count; i < _size; i++) {
+            delete this[i];
+        }
+    } else if (count > _size) {
+        for (auto i = _size; i < count; i++) {
+            new (this[i]) value_type(value);
+        }
+    }
+    _size = count;
+}
+
+#pragma mark - Specialization for empty stack buffer
+
+namespace details {
+
+template <typename size_type, unsigned int element_size>
+    requires std::unsigned_integral<size_type>
+void *realloc_vector(void *buffer, size_type *size, size_type preferred_new_size) {
+    if (preferred_new_size == 0) {
+        *size = 0;
+        free(buffer);
+        return nullptr;
+    }
+
+    size_t new_size_bytes = malloc_good_size(preferred_new_size * element_size);
+    size_type new_size = new_size_bytes / element_size;
+    if (new_size == *size) {
+        // nothing to do
+        return buffer;
+    }
+
+    void *new_buffer = realloc(buffer, new_size_bytes);
+    if (!new_buffer) {
+        precondition_failure("allocation failure");
+    }
+    *size = new_size;
+    return new_buffer;
+}
+
+} // namespace details
+
+template <typename T, typename size_type>
+    requires std::unsigned_integral<size_type>
+vector<T, 0, size_type>::~vector() {
+    for (auto i = 0; i < _size; i++) {
+        delete this[i];
+    }
+    if (_buffer) {
+        free(_buffer);
+    }
+}
+
+template <typename T, typename size_type>
+    requires std::unsigned_integral<size_type>
+void vector<T, 0, size_type>::reserve_slow(size_type new_cap) {
+    size_type effective_new_cap = max(capacity() * 1.5, new_cap);
+    _buffer = details::realloc_vector(_buffer, &_capacity, effective_new_cap);
+}
+
+template <typename T, typename size_type>
+    requires std::unsigned_integral<size_type>
+void vector<T, 0, size_type>::reserve(size_type new_cap) {
+    if (new_cap <= capacity()) {
+        return;
+    }
+    reserve_slow(new_cap);
+}
+
+template <typename T, typename size_type>
+    requires std::unsigned_integral<size_type>
+void vector<T, 0, size_type>::clear() {
+    for (auto i = 0; i < _size; i++) {
+        delete this[i];
+    }
+    _size = 0;
+}
+
+template <typename T, typename size_type>
+    requires std::unsigned_integral<size_type>
+void vector<T, 0, size_type>::push_back(const T &value) {
+    reserve(_size + 1);
+    new (this[_size]) value_type(value);
+    _size += 1;
+}
+
+template <typename T, typename size_type>
+    requires std::unsigned_integral<size_type>
+void vector<T, 0, size_type>::push_back(T &&value) {
+    reserve(_size + 1);
+    new (this[_size]) value_type(std::move(value));
+    _size += 1;
+}
+
+template <typename T, typename size_type>
+    requires std::unsigned_integral<size_type>
+void vector<T, 0, size_type>::pop_back() {
+    assert(size() > 0);
+    delete this[_size - 1];
+    _size -= 1;
+}
+
+template <typename T, typename size_type>
+    requires std::unsigned_integral<size_type>
+void vector<T, 0, size_type>::resize(size_type count) {
+    reserve(count);
+    if (count < _size) {
+        for (auto i = count; i < _size; i++) {
+            delete this[i];
+        }
+    } else if (count > _size) {
+        for (auto i = _size; i < count; i++) {
+            new (this[i]) value_type();
+        }
+    }
+    _size = count;
+}
+
+template <typename T, typename size_type>
+    requires std::unsigned_integral<size_type>
+void vector<T, 0, size_type>::resize(size_type count, const value_type &value) {
+    reserve(count);
+    if (count < _size) {
+        for (auto i = count; i < _size; i++) {
+            delete this[i];
+        }
+    } else if (count > _size) {
+        for (auto i = _size; i < count; i++) {
+            new (this[i]) value_type(value);
+        }
+    }
+    _size = count;
+}
+
+} // namespace AG
---
+++ Sources/ComputeCxx/Vector/Vector.h
@@ -0,0 +1,149 @@
+#pragma once
+
+#include <CoreFoundation/CFBase.h>
+#include <concepts>
+#include <iterator>
+
+CF_ASSUME_NONNULL_BEGIN
+
+namespace AG {
+
+template <typename T, unsigned int _stack_size = 0, typename size_type = std::size_t>
+    requires std::unsigned_integral<size_type>
+class vector {
+  private:
+    T _stack_buffer[_stack_size];
+    T *_Nullable _buffer = nullptr;
+    size_type _size = 0;
+    size_type _capacity = _stack_size;
+
+    void reserve_slow(size_type new_cap);
+
+  public:
+    using value_type = T;
+    using reference = value_type &;
+    using const_reference = const value_type &;
+    using iterator = value_type *_Nonnull;
+    using const_iterator = const value_type *_Nonnull;
+    using reverse_iterator = std::reverse_iterator<iterator>;
+    using const_reverse_iterator = std::reverse_iterator<const_iterator>;
+
+    ~vector();
+
+    // Element access
+
+    reference operator[](size_type pos) { return data()[pos]; };
+    const_reference operator[](size_type pos) const { return data()[pos]; };
+
+    reference front() { return &data()[0]; };
+    const_reference front() const { return *&data()[0]; };
+    reference back() { return *&data()[_size - 1]; };
+    const_reference back() const { return *&data()[_size - 1]; };
+
+    T *_Nonnull data() { return _buffer != nullptr ? _buffer : _stack_buffer; };
+    const T *_Nonnull data() const { return _buffer != nullptr ? _buffer : _stack_buffer; };
+
+    // Iterators
+
+    iterator begin() { return iterator(&data()[0]); };
+    const_iterator cbegin() const { return const_iterator(&data()[0]); };
+    iterator end() { return iterator(&data()[_size]); };
+    const_iterator cend() const { return const_iterator(&data()[_size]); };
+
+    reverse_iterator rbegin() { return std::reverse_iterator(end()); };
+    const_reverse_iterator crbegin() const { return std::reverse_iterator(cend()); };
+    reverse_iterator rend() { return std::reverse_iterator(begin()); };
+    const_reverse_iterator crend() const { return std::reverse_iterator(cbegin()); };
+
+    // Capacity
+
+    bool empty() const { return _size == 0; };
+    size_type size() const { return _size; };
+    void reserve(size_type new_cap);
+    size_type capacity() const { return _capacity; };
+
+    // Modifiers
+
+    void clear();
+
+    void push_back(const T &value);
+    void push_back(T &&value);
+    void pop_back();
+
+    void resize(size_type count);
+    void resize(size_type count, const value_type &value);
+};
+
+static_assert(std::contiguous_iterator<vector<int>::iterator>);
+static_assert(std::contiguous_iterator<vector<int>::const_iterator>);
+
+// MARK: Specialization for empty stack buffer
+
+template <typename T, typename size_type>
+    requires std::unsigned_integral<size_type>
+class vector<T, 0, size_type> {
+  private:
+    T *_Nullable _buffer = nullptr;
+    size_type _size = 0;
+    size_type _capacity = 0;
+
+    void reserve_slow(size_type new_cap);
+
+  public:
+    using value_type = T;
+    using reference = value_type &;
+    using const_reference = const value_type &;
+    using iterator = value_type *_Nonnull;
+    using const_iterator = const value_type *_Nonnull;
+    using reverse_iterator = std::reverse_iterator<iterator>;
+    using const_reverse_iterator = std::reverse_iterator<const_iterator>;
+
+    ~vector();
+
+    // Element access
+
+    reference operator[](size_type pos) { return data()[pos]; };
+    const_reference operator[](size_type pos) const { return data()[pos]; };
+
+    reference front() { return &data()[0]; };
+    const_reference front() const { return *&data()[0]; };
+    reference back() { return *&data()[_size - 1]; };
+    const_reference back() const { return *&data()[_size - 1]; };
+
+    T *_Nonnull data() { return _buffer; };
+    const T *_Nonnull data() const { return _buffer; };
+
+    // Iterators
+
+    iterator begin() { return iterator(&data()[0]); };
+    const_iterator cbegin() const { return const_iterator(&data()[0]); };
+    iterator end() { return iterator(&data()[_size]); };
+    const_iterator cend() const { return const_iterator(&data()[_size]); };
+
+    reverse_iterator rbegin() { return std::reverse_iterator(end()); };
+    const_reverse_iterator crbegin() const { return std::reverse_iterator(cend()); };
+    reverse_iterator rend() { return std::reverse_iterator(begin()); };
+    const_reverse_iterator crend() const { return std::reverse_iterator(cbegin()); };
+
+    // Capacity
+
+    bool empty() const { return _size == 0; };
+    size_type size() const { return _size; };
+    void reserve(size_type new_cap);
+    size_type capacity() const { return _capacity; };
+
+    // Modifiers
+
+    void clear();
+
+    void push_back(const T &value);
+    void push_back(T &&value);
+    void pop_back();
+
+    void resize(size_type count);
+    void resize(size_type count, const value_type &value);
+};
+
+} // namespace AG
+
+CF_ASSUME_NONNULL_END

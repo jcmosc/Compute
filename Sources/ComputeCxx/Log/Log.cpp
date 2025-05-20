2708e30a0c25651ad14f617727b01a89325e5156
James Moschou <james.moschou@gmail.com>
2025-05-20 21:44:04 +0200

    Add AG::misc_log

---
+++ Sources/ComputeCxx/Log/Log.cpp
@@ -0,0 +1,10 @@
+#include "Log/Log.h"
+
+namespace AG {
+
+os_log_t misc_log() {
+    static os_log_t log = os_log_create("dev.incrematic.compute", "misc");
+    return log;
+}
+
+} // namespace AG
---
+++ Sources/ComputeCxx/Log/Log.h
@@ -0,0 +1,14 @@
+#pragma once
+
+#include <os/log.h>
+#include <CoreFoundation/CFBase.h>
+
+CF_ASSUME_NONNULL_BEGIN
+
+namespace AG {
+
+os_log_t misc_log();
+
+} // namespace AG
+
+CF_ASSUME_NONNULL_END

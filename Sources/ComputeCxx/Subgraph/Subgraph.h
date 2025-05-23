0ea83d50929e77ef1e67aa36e131bc37fca4e1ed
James Moschou <james.moschou@gmail.com>
2025-05-23 22:27:54 +0200

    Declare AG::Subgraph::to_cf()

--- Sources/ComputeCxx/Subgraph/Subgraph.h
+++ Sources/ComputeCxx/Subgraph/Subgraph.h
@@ -2,20 +2,30 @@
 
 #include <CoreFoundation/CFBase.h>
 
+#include "AGSubgraph.h"
 #include "Data/Zone.h"
 
 CF_ASSUME_NONNULL_BEGIN
 
+struct AGSubgraphStorage;
+
 namespace AG {
 
 class Graph;
+class SubgraphObject;
 
 class Subgraph : public data::zone {
   private:
+    SubgraphObject *_object;
     Graph *_graph;
 
   public:
-    Graph &graph() const { return *_graph; };
+    AGSubgraphStorage *to_cf() const;
+    static Subgraph *from_cf(AGSubgraphStorage *storage);
+
+    Graph *graph() const { return _graph; };
+
+    uint64_t context_id() const { return _context_id; }
 };
 
 } // namespace AG

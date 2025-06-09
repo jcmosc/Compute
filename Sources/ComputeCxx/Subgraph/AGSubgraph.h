fee81c509fc928e2b118403682ed8bc3c84a6ae8
James Moschou <james.moschou@gmail.com>
2025-06-09 13:30:36 +0200

    Reorder function declarations

--- Sources/ComputeCxx/Subgraph/AGSubgraph.h
+++ Sources/ComputeCxx/Subgraph/AGSubgraph.h
@@ -27,19 +27,6 @@ CF_EXPORT
 CF_REFINED_FOR_SWIFT
 void AGSubgraphSetCurrent(AGSubgraphRef _Nullable subgraph) CF_SWIFT_NAME(setter:AGSubgraphRef.current(_:));
 
-// MARK: Observers
-
-CF_EXPORT
-CF_REFINED_FOR_SWIFT
-uint64_t AGSubgraphAddObserver(AGSubgraphRef subgraph,
-                               void (*observer)(void *_Nullable context AG_SWIFT_CONTEXT) AG_SWIFT_CC(swift),
-                               void *_Nullable observer_context);
-
-CF_EXPORT
-CF_REFINED_FOR_SWIFT
-void AGSubgraphRemoveObserver(AGSubgraphRef subgraph, uint64_t observer_id)
-    CF_SWIFT_NAME(AGSubgraphRef.removeObserver(self:observerID:));
-
 // MARK: Graph Context
 
 CF_EXPORT
@@ -67,6 +54,19 @@ bool AGSubgraphIsValid(AGSubgraphRef subgraph) CF_SWIFT_NAME(getter:AGSubgraphRe
 CF_EXPORT
 CF_REFINED_FOR_SWIFT
 void AGSubgraphInvalidate(AGSubgraphRef subgraph) CF_SWIFT_NAME(AGSubgraphRef.invalidate(self:));
+
+// MARK: Observers
+
+CF_EXPORT
+CF_REFINED_FOR_SWIFT
+uint64_t AGSubgraphAddObserver(AGSubgraphRef subgraph,
+                               void (*observer)(void *_Nullable context AG_SWIFT_CONTEXT) AG_SWIFT_CC(swift),
+                               void *_Nullable observer_context);
+
+CF_EXPORT
+CF_REFINED_FOR_SWIFT
+void AGSubgraphRemoveObserver(AGSubgraphRef subgraph, uint64_t observer_id)
+    CF_SWIFT_NAME(AGSubgraphRef.removeObserver(self:observerID:));
 
 // MARK: Children
 

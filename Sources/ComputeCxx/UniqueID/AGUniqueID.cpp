b06889f7859b3a193d1ef9d7aece9e22ee74014a
James Moschou <james.moschou@gmail.com>
2026-01-05 15:36:14 +0100

    Make AGMakeUniqueID atomic

--- Sources/ComputeCxx/UniqueID/AGUniqueID.cpp
+++ Sources/ComputeCxx/UniqueID/AGUniqueID.cpp
@@ -1,6 +1,8 @@
-#include "ComputeCxx/AGUniqueID.h"
+#include <stdatomic.h>
+
+#include <ComputeCxx/AGUniqueID.h>
 
 AGUniqueID AGMakeUniqueID() {
-    static uint64_t counter = 0;
-    return ++counter;
+    static atomic_ulong counter = 1;
+    return atomic_fetch_add_explicit(&counter, 1, memory_order_relaxed);
 }

f1c72143865cabbb99f9c2f7069d72c5387b34cf
James Moschou <james.moschou@gmail.com>
2026-01-05 15:35:35 +0100

    fixup macro

--- Sources/ComputeCxx/Time/Time.h
+++ Sources/ComputeCxx/Time/Time.h
@@ -1,8 +1,14 @@
 #include <stdint.h>
 
+#include "ComputeCxx/AGBase.h"
+
+AG_ASSUME_NONNULL_BEGIN
+
 namespace AG {
 
 double current_time(void);
 double absolute_time_to_seconds(uint64_t ticks);
 
 }
+
+AG_ASSUME_NONNULL_END

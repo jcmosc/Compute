c47fa2e3be4ddf9f757a56346198752e9dd0ac4f
James Moschou <james.moschou@gmail.com>
2026-06-17 22:59:07 +0200

    fixup env vars

--- Tests/ComputeTests/Shims.swift
+++ Tests/ComputeTests/Shims.swift
@@ -1,1 +1,5 @@
 @_exported public import Compute
+
+let prefetchLayoutsEnvironmentVariable = "IAG_PREFETCH_LAYOUTS"
+let asyncLayoutsEnvironmentVariable = "IAG_ASYNC_LAYOUTS"
+let printLayoutsEnvironmentVariable = "IAG_PRINT_LAYOUTS"

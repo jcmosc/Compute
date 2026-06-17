e0837fc73252b2f3cdf3797ddbedf492d86dd768
James Moschou <james.moschou@gmail.com>
2026-06-17 12:11:28 +0200

    Add format.sh

---
+++ Scripts/format.sh
@@ -0,0 +1,9 @@
+#!/bin/zsh
+
+set -e
+
+echo "Formatting Swift sources in $(pwd)"
+
+# Run the format / lint commands
+git ls-files -z '*.swift' | xargs -0 swift format format --parallel --in-place
+git ls-files -z '*.swift' | xargs -0 swift format lint --strict --parallel

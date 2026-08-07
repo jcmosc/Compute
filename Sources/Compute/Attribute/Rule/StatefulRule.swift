02210719b76dfba36fb849626daca22ecae72bc2
James Moschou <james.moschou@gmail.com>
2026-08-07 16:09:41 +0200

    Implement Rule.bodyChanged and StatefulRule.bodyChanged

--- Sources/Compute/Attribute/Rule/StatefulRule.swift
+++ Sources/Compute/Attribute/Rule/StatefulRule.swift
@@ -1,18 +1,15 @@
 import ComputeCxx
 
 public protocol StatefulRule: _AttributeBody {
-
     associatedtype Value
 
     static var initialValue: Value? { get }
     mutating func updateValue()
-
 }
 
 extension StatefulRule {
-
     public static var initialValue: Value? {
-        return nil
+        nil
     }
 
     public static func _updateDefault(_ default: UnsafeMutableRawPointer) {
@@ -26,14 +23,14 @@ extension StatefulRule {
 }
 
 extension StatefulRule {
-
     public static func _update(_ self: UnsafeMutableRawPointer, attribute: AnyAttribute) {
         let rule = self.assumingMemoryBound(to: Self.self)
         rule.pointee.updateValue()
     }
 
+    @_alwaysEmitIntoClient
     public var bodyChanged: Bool {
-        fatalError("not implemented")
+        AnyAttribute.currentWasModified
     }
 
     public var value: Value {
@@ -50,15 +47,14 @@ extension StatefulRule {
     }
 
     public var hasValue: Bool {
-        return context.hasValue
+        context.hasValue
     }
 
     public var attribute: Attribute<Value> {
-        return Attribute<Value>(identifier: AnyAttribute.current!)
+        Attribute<Value>(identifier: AnyAttribute.current!)
     }
 
     public var context: RuleContext<Value> {
-        return RuleContext<Value>(attribute: attribute)
+        RuleContext<Value>(attribute: attribute)
     }
-
 }

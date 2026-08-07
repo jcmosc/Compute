38d2999b75c29f590012033d31bc9e4b415481a9
James Moschou <james.moschou@gmail.com>
2026-08-07 16:11:32 +0200

    Update compatibility framework headers

--- CompatibilityTesting/Frameworks/AttributeGraph.xcframework/maccatalyst-arm64_arm64e_x86_64/AttributeGraph.framework/Headers/AGDescription.h
+++ CompatibilityTesting/Frameworks/AttributeGraph.xcframework/maccatalyst-arm64_arm64e_x86_64/AttributeGraph.framework/Headers/AGDescription.h
@@ -3,7 +3,11 @@
 #include <AttributeGraph/AGBase.h>
 
 #if TARGET_OS_MAC
+#ifdef __OBJC__
+#import <Foundation/Foundation.h>
+#else
 #include <CoreFoundation/CFString.h>
+#endif
 #else
 #include <SwiftCorelibsCoreFoundation/CFString.h>
 #endif
@@ -16,19 +20,23 @@ AG_EXTERN_C_BEGIN
 
 #if TARGET_OS_MAC
 
+#ifdef __OBJC__
+typedef NSString *AGDescriptionOption AG_SWIFT_STRUCT AG_SWIFT_NAME(DescriptionOption);
+#else
 typedef CFStringRef AGDescriptionOption AG_SWIFT_STRUCT AG_SWIFT_NAME(DescriptionOption);
+#endif
 
 AG_EXPORT
-const AGDescriptionOption AGDescriptionFormat AG_SWIFT_NAME(AGDescriptionOption.format);
+const AGDescriptionOption AGDescriptionFormat AG_SWIFT_NAME(DescriptionOption.format);
 
 AG_EXPORT
-const AGDescriptionOption AGDescriptionMaxFrames AG_SWIFT_NAME(AGDescriptionOption.maxFrames);
+const AGDescriptionOption AGDescriptionMaxFrames AG_SWIFT_NAME(DescriptionOption.maxFrames);
 
 AG_EXPORT
-const AGDescriptionOption AGDescriptionIncludeValues AG_SWIFT_NAME(AGDescriptionOption.includeValues);
+const AGDescriptionOption AGDescriptionIncludeValues AG_SWIFT_NAME(DescriptionOption.includeValues);
 
 AG_EXPORT
-const AGDescriptionOption AGDescriptionTruncationLimit AG_SWIFT_NAME(AGDescriptionOption.truncationLimit);
+const AGDescriptionOption AGDescriptionTruncationLimit AG_SWIFT_NAME(DescriptionOption.truncationLimit);
 
 #endif
 

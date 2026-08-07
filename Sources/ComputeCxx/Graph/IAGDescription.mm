fd5ec66dc2c14cf19ac7b8310881126e1077deb4
James Moschou <james.moschou@gmail.com>
2026-08-07 14:42:15 +0200

    Make IAGDescriptionOption an alias for NSString * when Objective-C is available

--- Sources/ComputeCxx/Graph/IAGDescription.mm
+++ Sources/ComputeCxx/Graph/IAGDescription.mm
@@ -1,8 +1,13 @@
 #include "ComputeCxx/IAGDescription.h"
 
 #if TARGET_OS_MAC
-const IAGDescriptionOption IAGDescriptionFormat = CFSTR("format");
-const IAGDescriptionOption IAGDescriptionMaxFrames = CFSTR("max-frames");
-const IAGDescriptionOption IAGDescriptionIncludeValues = CFSTR("include-values");
-const IAGDescriptionOption IAGDescriptionTruncationLimit = CFSTR("truncation-limit");
+#ifdef __OBJC__
+#define IAG_BRIDGE(v) (__bridge NSString *)(v)
+#else
+#define IAG_BRIDGE(v) (v)
+#endif
+const IAGDescriptionOption IAGDescriptionFormat = IAG_BRIDGE(CFSTR("format"));
+const IAGDescriptionOption IAGDescriptionMaxFrames = IAG_BRIDGE(CFSTR("max-frames"));
+const IAGDescriptionOption IAGDescriptionIncludeValues = IAG_BRIDGE(CFSTR("include-values"));
+const IAGDescriptionOption IAGDescriptionTruncationLimit = IAG_BRIDGE(CFSTR("truncation-limit"));
 #endif
